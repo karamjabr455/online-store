a7375d15b308ff4fde18209248b3c8a1ba4e30b4
karamjabr455 <abua33012@gmail.com>
2024-08-22 20:46:46 -0700

    Update App.js to set SignUp as the default page

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import './i18n';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; 
 import SignUp from './SignUp';
 import LogIn from './LogIn';
 import Verify from "./Verify";
 import Welcome from "./Welcome";
-import Productsdetails from './Productsdetails'
+import Productsdetails from './Productsdetails';
 import Home from './components/Home'; 
 import OfferPage from './OfferPage';
 import Cart from './Cart';
-import Checkout from './Checkout'
+import Checkout from './Checkout';
 import { ThemeProvider } from './ThemeContext'; 
-
-
 
 function App() {
   return (
@@ -27,18 +24,17 @@ function App() {
       <Router> 
         <div className="app-container">
           <Routes>
-           
+          
+            <Route path="/" element={<Navigate to="/signup" />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<LogIn />} />
             <Route path="/verify" element={<Verify />} />
             <Route path="/welcome" element={<Welcome />} />
             <Route path="/home" element={<Home />} />
             <Route path="/products" element={<Productsdetails />} />
-            <Route path="/offer" element={<OfferPage  />} />
+            <Route path="/offer" element={<OfferPage />} />
             <Route path="/Cart" element={<Cart />} />  
             <Route path="/Checkout" element={<Checkout />} /> 
-         
-
           </Routes>
         </div>
       </Router>
